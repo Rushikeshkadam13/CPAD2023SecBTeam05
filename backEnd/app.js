ca824313ed23adfa69a18360aba7011875f93761
RohitPatil39 <127203656+RohitPatil39@users.noreply.github.com>
2023-11-25 18:26:35 +0530

    adding helmet for security

--- backEnd/app.js
+++ backEnd/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-// const configiration = require("../config.js");
+const helmet = require("helmet");
 
 const mongoose = require("mongoose");
 const spiltterRouter = require("./routes/routes.js");
@@ -12,6 +12,11 @@ const uri =
   process.env.PASSWORD +
   "@cluster0.z70uixj.mongodb.net/ExpenseSplitter?retryWrites=true&w=majority";
 
+app.use(helmet());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to SPlitExpences");
+});
 
 mongoose
   .connect(uri, {
